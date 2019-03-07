755db61f33bb58b621844a0cef37b99c4f8cf25c
Tess <tess.thornley@gmail.com>
2019-03-07 18:22:59 +0000

    two sorting activities added in one js doc

---
+++ w1d4/sorting.js
@@ -0,0 +1,28 @@
+var array = [10, 2, 5, 1, 9];
+array.sort(function(a,b) {
+  return a - b
+})
+
+console.log(array)
+
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a, b) {
+  var nameA = a.name;
+  var nameB = b.name;
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+return b.age - a.age
+});
+
+
+console.log(students)
